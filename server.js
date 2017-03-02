e48310aef6edd81729267b74074c4e31bbf29edb
ranyehushua <ranyehushua@gmail.com>
2017-03-02 11:32:10 -0700

    Removed newServer and updated server with fixed code

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ let smogJSON = {
   smogLevel: 4
 }
 
-const respondWithSmog = (res, res, next) => {
-  res.send(smogLevel);
+// Fixed duplicate res, res and changed smogLevel to smogJSON to solve reference error
+const respondWithSmog = (req, res, next) => {
+  res.send(smogJSON);
   next();
 }
 
+// I forgot, server.use is for middleware as opposed to request handling. So I changed it to get and specified the root path
 const server = restify.createServer();
-server.use(respondWithSmog);
+server.get('/', respondWithSmog);
 
 server.listen(3030, () => {
   console.log('%s listening at %s', server.name, server.url);
