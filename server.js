f08e2c8d85d283f41f1e13fa8ef25af665436766
ranyehushua <ranyehushua@gmail.com>
2017-03-02 12:26:20 -0700

    removed comments from previous server fix

--- server.js
+++ server.js
@@ -5,13 +5,11 @@ let smogJSON = {
   smogLevel: 4
 }
 
-// Fixed duplicate res, res and changed smogLevel to smogJSON to solve reference error
 const respondWithSmog = (req, res, next) => {
   res.send(smogJSON);
   next();
 }
 
-// I forgot, server.use is for middleware as opposed to request handling. So I changed it to get and specified the root path
 const server = restify.createServer();
 server.get('/', respondWithSmog);
 
